47f251809d1f1b5ddd619e511b49c46e06484f83
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-02-10 20:29:45 -0300

    Improve request builder usage

--- src/tests/controllers/movie.controller.test.js
+++ src/tests/controllers/movie.controller.test.js
@@ -16,39 +16,30 @@ afterAll(async () => {
 
 const RequestBuilderInstance = new RequestBuilder();
 
+const post = (path) => RequestBuilderInstance.newRequest().post(path);
+// const deletion = (path) => RequestBuilderInstance.newRequest().delete(path);
+
 describe("MOVIE CONTROLLER", () => {
   describe("CREATE MOVIE", () => {
     it("Should call MovieService.create once if it is a valid movie", async () => {
       const spy = jest.spyOn(MovieService, "create");
       const body = createMoviePayload();
-      await RequestBuilderInstance.newRequest()
-        .post("/movie")
-        .send(body)
-        .build();
+      await post("/movie").send(body).build();
       expect(spy).toHaveBeenCalledTimes(1);
       expect(spy).toHaveBeenCalledWith(body);
     });
 
     it("Should return the json data of the created movie with status 200", async () => {
       const body = createMoviePayload();
-      const result = await RequestBuilderInstance.newRequest()
-        .post("/movie")
-        .send(body)
-        .build();
+      const result = await post("/movie").send(body).build();
       expect(result.status).toBe(200);
       expect(result.body).toEqual(expect.objectContaining(body));
       expect(result.body).toHaveProperty("id");
     });
 
     it("Should return a 400 status code if the request body is incomplete", async () => {
-      await RequestBuilderInstance.newRequest()
-        .post("/movie")
-        .send(createMoviePayload())
-        .build();
-      const result = await RequestBuilderInstance.newRequest()
-        .post("/movie")
-        .send({ title: "Movie" })
-        .build();
+      await post("/movie").send(createMoviePayload()).build();
+      const result = await post("/movie").send({ title: "Movie" }).build();
       expect(result.status).toBe(400);
       expect(result.body).toEqual(messageCodeMap.get(400));
     });
@@ -59,13 +50,9 @@ describe("MOVIE CONTROLLER", () => {
       // const spy = jest.spyOn(MovieService, "create");
 
       const body = createMoviePayload();
-      await RequestBuilderInstance.newRequest()
-        .post("/movie")
-        .send(body)
-        .build();
+      await post("/movie").send(body).build();
 
-      const result = await RequestBuilderInstance.newRequest()
-        .post("/movie/id/review")
+      const result = await post("/movie/id/review")
         .withValidRegularUserToken()
         .send()
         .build();
@@ -74,8 +61,7 @@ describe("MOVIE CONTROLLER", () => {
       // expect(result.body).toEqual(messageCodeMap.get(401));
     });
     it("Should not allow admin users to create reviews", async () => {
-      const result = await RequestBuilderInstance.newRequest()
-        .post("/movie/id/review")
+      const result = await post("/movie/id/review")
         .withValidAdminToken()
         .build();
       expect(result.status).toBe(401);
--- src/tests/controllers/user.controller.test.js
+++ src/tests/controllers/user.controller.test.js
@@ -19,24 +19,22 @@ afterAll(async () => {
 
 const RequestBuilderInstance = new RequestBuilder();
 
+const post = (path) => RequestBuilderInstance.newRequest().post(path);
+const patch = (path) => RequestBuilderInstance.newRequest().patch(path);
+const deletion = (path) => RequestBuilderInstance.newRequest().delete(path);
+
 describe("USER CONTROLLER", () => {
   describe("CREATE USER", () => {
     it("Should call UserService.create once if it is a valid user", async () => {
       const spy = jest.spyOn(UserService, "create");
       const body = createUserPayload();
-      await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(body)
-        .build();
+      await post("/user").send(body).build();
       expect(spy).toHaveBeenCalledTimes(1);
       expect(spy).toHaveBeenCalledWith(body);
     });
 
     it("Should return the json data (no password) of the created user with status 200", async () => {
-      const result = await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
+      const result = await post("/user").send(createUserPayload()).build();
       expect(result.status).toBe(200);
       expect(result.body).toEqual(
         expect.objectContaining(createUserPayloadWithoutPassword())
@@ -46,27 +44,15 @@ describe("USER CONTROLLER", () => {
     });
 
     it("Should return a 409 status code if the user already exists", async () => {
-      await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
-      const result = await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
+      await post("/user").send(createUserPayload()).build();
+      const result = await post("/user").send(createUserPayload()).build();
       expect(result.status).toBe(409);
       expect(result.body).toEqual(messageCodeMap.get(409));
     });
 
     it("Should return a 400 status code if the request body is incomplete", async () => {
-      await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
-      const result = await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send({ name: "Pedro" })
-        .build();
+      await post("/user").send(createUserPayload()).build();
+      const result = await post("/user").send({ name: "Pedro" }).build();
       expect(result.status).toBe(400);
       expect(result.body).toEqual(messageCodeMap.get(400));
     });
@@ -77,15 +63,9 @@ describe("USER CONTROLLER", () => {
       const spy = jest.spyOn(UserService, "update");
       const {
         body: { id },
-      } = await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
+      } = await post("/user").send(createUserPayload()).build();
       const body = { name: "Pedro" };
-      await RequestBuilderInstance.newRequest()
-        .patch(`/user/${id}`)
-        .send(body)
-        .build();
+      await patch(`/user/${id}`).send(body).build();
       expect(spy).toHaveBeenCalledTimes(1);
       expect(spy).toHaveBeenCalledWith(id, body);
     });
@@ -93,15 +73,9 @@ describe("USER CONTROLLER", () => {
     it("Should return the json data (no password) of the updated user with status 200", async () => {
       const {
         body: { id },
-      } = await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
+      } = await post("/user").send(createUserPayload()).build();
       const body = { name: "Pedro" };
-      const result = await RequestBuilderInstance.newRequest()
-        .patch(`/user/${id}`)
-        .send(body)
-        .build();
+      const result = await patch(`/user/${id}`).send(body).build();
       expect(result.status).toBe(200);
       expect(result.body).toEqual(
         expect.objectContaining({
@@ -116,19 +90,12 @@ describe("USER CONTROLLER", () => {
     it("Should return 409 status if new email already exists", async () => {
       const {
         body: { id },
-      } = await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
+      } = await post("/user").send(createUserPayload()).build();
       const body = { email: "myOther@email.com" };
-      await RequestBuilderInstance.newRequest()
-        .post("/user")
+      await post("/user")
         .send({ ...createUserPayload(), ...body })
         .build();
-      const result = await RequestBuilderInstance.newRequest()
-        .patch(`/user/${id}`)
-        .send(body)
-        .build();
+      const result = await patch(`/user/${id}`).send(body).build();
       expect(result.status).toBe(409);
       expect(result.body).toEqual(messageCodeMap.get(409));
     });
@@ -136,25 +103,16 @@ describe("USER CONTROLLER", () => {
     it("Should return 400 status if tries to update unupdatable field", async () => {
       const {
         body: { id },
-      } = await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
+      } = await post("/user").send(createUserPayload()).build();
       const body = { isActive: false };
-      const result = await RequestBuilderInstance.newRequest()
-        .patch(`/user/${id}`)
-        .send(body)
-        .build();
+      const result = await patch(`/user/${id}`).send(body).build();
       expect(result.status).toBe(400);
       expect(result.body).toEqual(messageCodeMap.get(400));
     });
 
     it("Should return 404 status if user is not found", async () => {
       const body = { name: "Pedro" };
-      const result = await RequestBuilderInstance.newRequest()
-        .patch(`/user/fakeId`)
-        .send(body)
-        .build();
+      const result = await patch(`/user/fakeId`).send(body).build();
       expect(result.status).toBe(404);
       expect(result.body).toEqual(messageCodeMap.get(404));
     });
@@ -164,21 +122,14 @@ describe("USER CONTROLLER", () => {
     it("Should delete an existing user", async () => {
       const {
         body: { id },
-      } = await RequestBuilderInstance.newRequest()
-        .post("/user")
-        .send(createUserPayload())
-        .build();
-      const result = await RequestBuilderInstance.newRequest()
-        .delete(`/user/${id}`)
-        .build();
+      } = await post("/user").send(createUserPayload()).build();
+      const result = await deletion(`/user/${id}`).build();
       expect(result.status).toBe(200);
       expect(result.body).toEqual(messageCodeMap.get(200));
     });
 
     it("Should return 404 status if user is not found", async () => {
-      const result = await RequestBuilderInstance.newRequest()
-        .delete(`/user/fakeId`)
-        .build();
+      const result = await deletion(`/user/fakeId`).build();
       expect(result.status).toBe(404);
       expect(result.body).toEqual(messageCodeMap.get(404));
     });
